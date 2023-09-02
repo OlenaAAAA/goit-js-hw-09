84e2aadcce60fdcda1f070b0aa360fa6d64c99c7
OlenaAAAA <130498592+OlenaAAAA@users.noreply.github.com>
2023-09-02 20:06:45 +0300

    1st commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
 
+// УВАГА
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor
+
+
+
+
+
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+
+
+startBtn.addEventListener("click", onStart);
+stopBtn.addEventListener("click", onStop);
+
+
+function onStart() {
+    idInterval = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    startBtn.setAttribute("disabled", true);
+}
+
+function onStop() {
+    startBtn.disabled = false;
+    clearInterval(idInterval);  
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,111 @@
+// Напиши скрипт таймера, який здійснює зворотний відлік до певної дати.
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+
+
+const inputEl = document.querySelector("#datetime-picker");
+const startBTN = document.querySelector("button[data-start]");
+const daysEl = document.querySelector("[data-days]");
+const hoursEl = document.querySelector("[data-hours]");
+const minutesEl = document.querySelector("[data-minutes]");
+const secondsEl = document.querySelector("[data-seconds]");
+const timerEl = document.querySelector(".timer");
+const bodyEl = document.querySelector('body');
+
+bodyEl.style.backgroundColor = "#00FF7F";
+timerEl.style.dispayFlex;
+inputEl.style.backgroundColor = "#00F60F";
+
+
+startBTN.disabled = true;
+const currentDate = new Date();
+
+
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    // const selectedDate = selectedDates[0];
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < currentDate) {
+        return window.alert("Please choose a date in the future");
+        } else {
+          startBTN.disabled = false;
+    
+      // startTimer()
+      
+//  const date = Date.now();
+      // console.log(currentDate.getTime());
+      // const delta = selectedDates[0] - currentDate;
+      // console.log(delta);
+      // console.log(convertMs(delta));
+    };
+   },
+};
+ 
+flatpickr(inputEl, options);
+
+startBTN.addEventListener("click", startTimer);
+
+
+function startTimer() {
+
+  const selectedDate = new Date(inputEl.value);
+  
+  intervalId = setInterval(() => {
+    let currentDate = new Date();
+    let delta = selectedDate.getTime() - currentDate.getTime();
+      
+    if (delta <= 0) {
+      clearInterval(intervalId);
+    }
+
+    setTheTimer(delta)
+      
+    }, 1000);
+  };
+
+   
+
+function addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
+};
+
+
+function setTheTimer(delta) {
+    const { days, hours, minutes, seconds } = convertMs(delta);
+    daysEl.textContent = `${addLeadingZero(days)}`;
+    hoursEl.textContent = `${addLeadingZero(hours)}`;
+    minutesEl.textContent = `${addLeadingZero(minutes)}`;
+    secondsEl.textContent = `${addLeadingZero(seconds)}`;
+};
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
