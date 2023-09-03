7810a8e19d68d124549cd7f7bc9f143039198f0a
OlenaAAAA <130498592+OlenaAAAA@users.noreply.github.com>
2023-09-03 11:32:00 +0000

    Deploying to gh-pages from @ OlenaAAAA/goit-js-hw-09@f238cecb90d0143d8d9ec0dce8d3ad896351bdf1 🚀

--- 01-color-switcher.a1927f1f.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){idInterval=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(idInterval)}));
-//# sourceMappingURL=01-color-switcher.a1927f1f.js.map
