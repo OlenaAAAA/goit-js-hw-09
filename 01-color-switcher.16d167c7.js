dbae1389ee2373b5c882f7b49665fe28cddb75f5
OlenaAAAA <130498592+OlenaAAAA@users.noreply.github.com>
2023-09-04 09:43:44 +0000

    Deploying to gh-pages from @ OlenaAAAA/goit-js-hw-09@e0ab3489c3da25e54b48fe555bb04410e2c08ad6 🚀

--- 01-color-switcher.16d167c7.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");t.style.backgroundColor="lightblue",t.addEventListener("click",(function(){idInterval=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(idInterval)}));
-//# sourceMappingURL=01-color-switcher.16d167c7.js.map
---
+++ 01-color-switcher.87b599bb.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");t.style.backgroundColor="lightblue",t.addEventListener("click",(function(){n=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));let n=null;
+//# sourceMappingURL=01-color-switcher.87b599bb.js.map
